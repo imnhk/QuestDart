6179896ccf006437f9a1cb90bfa9c77a84f2ca73
Nam-hyeon Kim <koloke1712@gmail.com>
2019-11-03 12:33:51 +0900

    Add project

---
+++ Assets/Scripts/DartDirection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DartDirection : MonoBehaviour
+{
+    public GameObject gameManagerObj;
+
+    private GameManager gameManager;
+    private Rigidbody rb;
+    private OVRGrabbable grabbable;
+    private Collider col;
+    private ParticleSystem particle;
+
+    private bool isStuck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        gameManager = gameManagerObj.GetComponent<GameManager>();
+        rb = GetComponent<Rigidbody>();
+        grabbable = GetComponent<OVRGrabbable>();
+        col = GetComponent<Collider>();
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude > 1f && !grabbable.isGrabbed)
+        {
+            this.transform.rotation = Quaternion.LookRotation(new Vector3(-rb.velocity.z, rb.velocity.y, rb.velocity.x), transform.up);
+            isStuck = false;
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Board" && !isStuck)
+        {
+            isStuck = true;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            rb.useGravity = false;
+
+            particle.Play();
+
+            float dist = Vector3.Distance(transform.position, gameManager.center.transform.position);
+            gameManager.score += (int)Mathf.Clamp(10 - dist * 10, 0f, 10f);
+        }
+    }
+
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Board")
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            rb.useGravity = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public Transform center;
+    public TextMeshProUGUI scoreText;
+
+    void Awake()
+    {
+        Physics.gravity = new Vector3(0, -5.0F, 0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+}
